3b2bdad5ee4d4d49471fa06f0ac5d56ad90895b6
Rebekah Ndanu <rebekah.ndanu@student.moringaschool.com>
2024-01-15 09:39:21 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -4,8 +4,35 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener("DOMContentLoaded", function () {
+  const errorModal = document.getElementById("modal");
+  errorModal.classList.add("hidden");
 
+  const hearts = document.querySelectorAll(".like-glyph");
 
+  hearts.forEach((heart) => {
+    heart.addEventListener("click", () => {
+      mimicServerCall()
+        .then(() => {
+          if (heart.classList.contains("activated-heart")) {
+            heart.classList.remove("activated-heart");
+            heart.innerText = EMPTY_HEART;
+          } else {
+            heart.classList.add("activated-heart");
+            heart.innerText = FULL_HEART;
+          }
+        })
+        .catch((error) => {
+          errorModal.classList.remove("hidden");
+          const modalMessage = document.getElementById("modal-message");
+          modalMessage.innerText = `Server Error: ${error}`;
+          setTimeout(() => {
+            errorModal.classList.add("hidden");
+          }, 3000);
+        });
+    });
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
